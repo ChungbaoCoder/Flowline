669feed0efaefd45068a21ffeac1ff8d9b11f61f
ChungBaoCoder <baocnqps40165@gmail.com>
2025-03-27 09:14:52 +0700

    Add Dbcontext and connection string

--- ps40165_Main/Program.cs
+++ ps40165_Main/Program.cs
@@ -1,6 +1,13 @@
+using Microsoft.EntityFrameworkCore;
+using ps40165_Main.Database;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
+builder.Services.AddDbContext<AppDbContext>(o =>
+{
+    o.UseSqlServer(builder.Configuration.GetConnectionString("Database"));
+});
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
